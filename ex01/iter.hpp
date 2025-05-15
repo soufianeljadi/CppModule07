29bee25e3b57808262f9083814a1e5d151cf73d7
Soufiane El Jadi <sel-jadi@e1r2p14.1337.ma>
2025-05-15 16:17:37 +0100

    done

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -12,5 +12,13 @@ void iter(T *array, size_t length, void (*f)(T &))
     }
 }
 
+template <typename T>
+void iter(const T *array, size_t length, void (*f)(const T &))
+{
+    for (size_t i = 0; i < length; ++i)
+    {
+        f(array[i]);
+    }
+}
 
 #endif

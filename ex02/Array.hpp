cdbc0a537bf2e99755693d77ce468ed56c179880
Soufiane El Jadi <sel-jadi@e1r2p14.1337.ma>
2025-05-15 16:17:06 +0100

    done

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -10,14 +10,63 @@ template <typename T> class Array
         T *array;
         unsigned int size;
     public:
-        Array();
-        Array(unsigned int n);
-        Array(const Array &src);
-        ~Array();
-        Array &operator=(const Array &src);
-        unsigned int getSize() const;
-        T &operator[](unsigned int index);
-        const T &operator[](unsigned int index) const;
+        Array() : array(NULL), size(0) {}
+        Array(unsigned int n) : size(n)
+        {
+            if (n > 0)
+                array = new T[n];
+            else
+                array = NULL;
+        }
+        Array(const Array &src) : size(src.size)
+        {
+            if (size > 0)
+            {
+                array = new T[size];
+                for (unsigned int i = 0; i < size; ++i)
+                    array[i] = src.array[i];
+            }
+            else
+                array = NULL;
+        }
+        ~Array()
+        {
+            delete[] array;
+        }
+        Array &operator=(const Array &src)
+        {
+            if (this != &src)
+            {
+                delete[] array;
+                size = src.size;
+                if (size > 0)
+                {
+                    array = new T[size];
+                    for (unsigned int i = 0; i < size; ++i)
+                        array[i] = src.array[i];
+                }
+                else
+                    array = NULL;
+            }
+            return *this;
+        }
+        unsigned int getSize() const
+        {
+            return size;
+        }
+        T &operator[](unsigned int index)
+        {
+            if (index >= size)
+                throw std::out_of_range("Index out of range");
+            return array[index];
+        }
+        const T &operator[](unsigned int index) const
+        {
+            if (index >= size)
+                throw std::out_of_range("Index out of range");
+            return array[index];
+        }
+
 };
 
 
